eb93e062d798edd35089fbf44582b25efb50d862
David W. Sosnowski <david@Davids-MBP.home>
2021-06-17 15:45:11 -0400

    Add updated ACE data

--- processed_data/ace_qc.R
+++ processed_data/ace_qc.R
@@ -157,19 +157,7 @@ df1$ACE13.frq.4 <- as.numeric( df1$ACE13.frq.4 )
 df1$ACE14.frq.4 <- as.numeric( df1$ACE14.frq.4 )
 df1$ACE15.frq.4 <- as.numeric( df1$ACE15.frq.4 )
 
-# create frequency x duration score
-df1$ACE1.fxd <- ( df1$ACE1.frq.4 / df1$ACE1.frq.2 )*df1$ACE1.frq.3
-df1$ACE2.fxd <- ( df1$ACE2.frq.4 / df1$ACE2.frq.2 )*df1$ACE2.frq.3
-df1$ACE7.fxd <- ( df1$ACE7.frq.4 / df1$ACE7.frq.2 )*df1$ACE7.frq.3
-df1$ACE9.fxd <- ( df1$ACE9.frq.4 / df1$ACE9.frq.2 )*df1$ACE9.frq.3
-df1$ACE10.fxd <- ( df1$ACE10.frq.4 / df1$ACE10.frq.2 )*df1$ACE10.frq.3
-df1$ACE11.fxd <- ( df1$ACE11.frq.4 / df1$ACE11.frq.2 )*df1$ACE11.frq.3
-df1$ACE12.fxd <- ( df1$ACE12.frq.4 / df1$ACE12.frq.2 )*df1$ACE12.frq.3
-df1$ACE13.fxd <- ( df1$ACE13.frq.4 / df1$ACE13.frq.2 )*df1$ACE13.frq.3
-df1$ACE14.fxd <- ( df1$ACE14.frq.4 / df1$ACE14.frq.2 )*df1$ACE14.frq.3
-df1$ACE15.fxd <- ( df1$ACE15.frq.4 / df1$ACE15.frq.2 )*df1$ACE15.frq.3
-
-# create same score, weighted by multiple daily occurrences
+# create frequency x duration score, weighted by multiple daily occurrences
 df1$ACE1.fxd.w <- ( ( df1$ACE1.frq.4 / df1$ACE1.frq.2 )*df1$ACE1.frq.3 )*df1$ACE1.frq.1
 df1$ACE2.fxd.w <- ( ( df1$ACE2.frq.4 / df1$ACE2.frq.2 )*df1$ACE2.frq.3 )*df1$ACE2.frq.1
 df1$ACE7.fxd.w <- ( ( df1$ACE7.frq.4 / df1$ACE7.frq.2 )*df1$ACE7.frq.3 )*df1$ACE7.frq.1
@@ -180,6 +168,58 @@ df1$ACE12.fxd.w <- ( ( df1$ACE12.frq.4 / df1$ACE12.frq.2 )*df1$ACE12.frq.3 )*df1
 df1$ACE13.fxd.w <- ( ( df1$ACE13.frq.4 / df1$ACE13.frq.2 )*df1$ACE13.frq.3 )*df1$ACE13.frq.1
 df1$ACE14.fxd.w <- ( ( df1$ACE14.frq.4 / df1$ACE14.frq.2 )*df1$ACE14.frq.3 )*df1$ACE14.frq.1
 df1$ACE15.fxd.w <- ( ( df1$ACE15.frq.4 / df1$ACE15.frq.2 )*df1$ACE15.frq.3 )*df1$ACE15.frq.1
+
+# create frequency x duration score, not weighted by multiple daily occurrences
+df1$ACE1.fxd <- df1$ACE1.fxd.w 
+df1$ACE1.fxd <- ifelse( df1$ACE1.frq.2 == 1, 
+                        ( ( df1$ACE1.frq.4 / df1$ACE1.frq.2 )*df1$ACE1.frq.3 ), 
+                        df1$ACE1.fxd )
+
+df1$ACE2.fxd <- df1$ACE2.fxd.w 
+df1$ACE2.fxd <- ifelse( df1$ACE2.frq.2 == 1, 
+                        ( ( df1$ACE2.frq.4 / df1$ACE2.frq.2 )*df1$ACE2.frq.3 ), 
+                        df1$ACE2.fxd )
+
+df1$ACE7.fxd <- df1$ACE7.fxd.w 
+df1$ACE7.fxd <- ifelse( df1$ACE7.frq.2 == 1, 
+                        ( ( df1$ACE7.frq.4 / df1$ACE7.frq.2 )*df1$ACE7.frq.3 ), 
+                        df1$ACE7.fxd )
+
+df1$ACE9.fxd <- df1$ACE9.fxd.w 
+df1$ACE9.fxd <- ifelse( df1$ACE9.frq.2 == 1, 
+                        ( ( df1$ACE9.frq.4 / df1$ACE9.frq.2 )*df1$ACE9.frq.3 ), 
+                        df1$ACE9.fxd )
+
+df1$ACE10.fxd <- df1$ACE10.fxd.w 
+df1$ACE10.fxd <- ifelse( df1$ACE10.frq.2 == 1, 
+                        ( ( df1$ACE10.frq.4 / df1$ACE10.frq.2 )*df1$ACE10.frq.3 ), 
+                        df1$ACE10.fxd )
+
+df1$ACE11.fxd <- df1$ACE11.fxd.w 
+df1$ACE11.fxd <- ifelse( df1$ACE11.frq.2 == 1, 
+                        ( ( df1$ACE11.frq.4 / df1$ACE11.frq.2 )*df1$ACE11.frq.3 ), 
+                        df1$ACE11.fxd )
+
+df1$ACE12.fxd <- df1$ACE12.fxd.w 
+df1$ACE12.fxd <- ifelse( df1$ACE12.frq.2 == 1, 
+                        ( ( df1$ACE12.frq.4 / df1$ACE12.frq.2 )*df1$ACE12.frq.3 ), 
+                        df1$ACE12.fxd )
+
+df1$ACE13.fxd <- df1$ACE13.fxd.w 
+df1$ACE13.fxd <- ifelse( df1$ACE13.frq.2 == 1, 
+                        ( ( df1$ACE13.frq.4 / df1$ACE13.frq.2 )*df1$ACE13.frq.3 ), 
+                        df1$ACE13.fxd )
+
+df1$ACE14.fxd <- df1$ACE14.fxd.w 
+df1$ACE14.fxd <- ifelse( df1$ACE14.frq.2 == 1, 
+                        ( ( df1$ACE14.frq.4 / df1$ACE14.frq.2 )*df1$ACE14.frq.3 ), 
+                        df1$ACE14.fxd )
+
+df1$ACE15.fxd <- df1$ACE15.fxd.w 
+df1$ACE15.fxd <- ifelse( df1$ACE15.frq.2 == 1, 
+                        ( ( df1$ACE15.frq.4 / df1$ACE15.frq.2 )*df1$ACE15.frq.3 ), 
+                        df1$ACE15.fxd )
+
 
 ### write out file for sharing
 write_csv( df1, file = "spring_2021_aces_cleaned.csv" )
