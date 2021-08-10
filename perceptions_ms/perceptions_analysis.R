eff958a51313a7e60b5152e8a4c444b54deed492
David Sosnowski <dsosnow1@jhu.edu>
2021-08-10 09:54:07 -0400

    Add sensitivity analysis

--- perceptions_ms/perceptions_analysis.R
+++ perceptions_ms/perceptions_analysis.R
@@ -21,7 +21,7 @@ df1 <- read_csv( "/Users/david/Desktop/aim_study/perceptions_ms/perceptions_fall
 # Outcome: GAD-7 total score
 ###################################
 
-summ( m5.adj <- lm( GAD7.rtot ~ Age + Race.b + m0f1 + factor( ses1 ) + 
+summ( m1.adj <- lm( GAD7.rtot ~ Age + Race.b + m0f1 + factor( ses1 ) + 
                       ACE.percep + ACE.count + ACE.percep:ACE.count, 
                     data = df1 ) )
 
@@ -29,12 +29,35 @@ summ( m5.adj <- lm( GAD7.rtot ~ Age + Race.b + m0f1 + factor( ses1 ) +
 #################################
 # Model 2
 # Outcome: PHQ-9
-# Predictor: ACE score*Impact
 #################################
 
 #PHQ-9 total score: adjusted model
-summ( m6.adj <- lm( PHQ9.rtot ~ Age + Race.b + m0f1 + factor( ses1 ) + 
+summ( m2.adj <- lm( PHQ9.rtot ~ Age + Race.b + m0f1 + factor( ses1 ) + 
                       ACE.percep + ACE.count + ACE.percep:ACE.count, 
                     data = df1 ) )
 
+
+
+###############################
+# Sensitivity Analysis
+# Inclusion of COVID covariate
+###############################
+
+###################################
+# Model 3
+# Outcome: GAD-7 total score
+###################################
+
+summ( m3.adj <- lm( GAD7.rtot ~ Age + Race.b + m0f1 + factor( ses1 ) + COVID5 + 
+                      ACE.percep + ACE.count + ACE.percep:ACE.count, data = df1 ) )
+
+
+#################################
+# Model 4
+# Outcome: PHQ-9
+#################################
+
+#PHQ-9 total score: adjusted model
+summ( m4.adj <- lm( PHQ9.rtot ~ Age + Race.b + m0f1 + factor( ses1 ) + COVID6 + 
+                      ACE.percep + ACE.count + ACE.percep:ACE.count, data = df1 ) )
 
