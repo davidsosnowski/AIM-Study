0312dbb7c900068b667ab13786b887625249835a
David Sosnowski <dsosnow1@jhu.edu>
2021-08-16 10:24:43 -0400

    Adding files for peer review

--- perceptions_ms/perceptions_analysis.R
+++ perceptions_ms/perceptions_analysis.R
@@ -1,9 +1,7 @@
-#######################################
-# AIM Study
-# Perceptions manuscript
-# Data analysis
-# David W. Sosnowski, PhD
-#######################################
+############################################################################
+# Perceived Negative Effects of Adverse Childhood Experiences 
+# as a Predictor of Depressive and Anxiety Symptoms Among College Students
+############################################################################
 
 getwd()
 
@@ -16,20 +14,20 @@ library( jtools )
 df1 <- read_csv( "/Users/david/Desktop/aim_study/perceptions_ms/perceptions_fall_spring_cleaned.csv" )
 
 
-###################################
+#############################
 # Model 1
 # Outcome: GAD-7 total score
-###################################
+#############################
 
 summ( m1.adj <- lm( GAD7.rtot ~ Age + Race.b + m0f1 + factor( ses1 ) + 
                       ACE.percep + ACE.count + ACE.percep:ACE.count, 
                     data = df1 ) )
 
 
-#################################
+#############################
 # Model 2
-# Outcome: PHQ-9
-#################################
+# Outcome: PHQ-9 total score
+#############################
 
 #PHQ-9 total score: adjusted model
 summ( m2.adj <- lm( PHQ9.rtot ~ Age + Race.b + m0f1 + factor( ses1 ) + 
@@ -39,25 +37,24 @@ summ( m2.adj <- lm( PHQ9.rtot ~ Age + Race.b + m0f1 + factor( ses1 ) +
 
 
 ###############################
-# Sensitivity Analysis
+# Sensitivity Analysis:
 # Inclusion of COVID covariate
 ###############################
 
-###################################
+#############################
 # Model 3
 # Outcome: GAD-7 total score
-###################################
+#############################
 
 summ( m3.adj <- lm( GAD7.rtot ~ Age + Race.b + m0f1 + factor( ses1 ) + COVID5 + 
                       ACE.percep + ACE.count + ACE.percep:ACE.count, data = df1 ) )
 
 
-#################################
+#############################
 # Model 4
-# Outcome: PHQ-9
-#################################
+# Outcome: PHQ-9 total score
+#############################
 
-#PHQ-9 total score: adjusted model
 summ( m4.adj <- lm( PHQ9.rtot ~ Age + Race.b + m0f1 + factor( ses1 ) + COVID6 + 
                       ACE.percep + ACE.count + ACE.percep:ACE.count, data = df1 ) )
 
